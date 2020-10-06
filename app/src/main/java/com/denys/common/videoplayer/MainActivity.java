7fbbf0f9c4c4ba3c26df5acaf47e7fb44ff114b8
Denys Tsaruk <denys.tsaruk@yandex.com>
2020-10-06 10:53:29 +0300

    remove download part

--- app/src/main/java/com/denys/common/videoplayer/MainActivity.java
+++ app/src/main/java/com/denys/common/videoplayer/MainActivity.java
@@ -1,27 +1,16 @@
 package com.denys.common.videoplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
-import android.util.Log;
-import android.view.View;
 import android.widget.MediaController;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_PERMISSION = 1;
@@ -33,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
     public static boolean isFirst = true;
 
     private android.widget.VideoView videoView;
-    private String videoPath;
     private String videoURL;
 
     @Override
@@ -44,14 +32,11 @@ public class MainActivity extends AppCompatActivity {
     }
     private void init () {
         videoURL = "http://34.210.212.174/Yp650OuYGo-20200926075619.mp4";
-        videoPath = Environment.getExternalStorageDirectory() + "/Download/test.mp4";
 
-        downloadFile();
-//        playVideo();
+        playVideo();
     }
     private void playVideo() {
         videoView.setVideoURI(Uri.parse(videoURL));
-        videoView.setVideoPath(videoPath);
 
         final MediaController ctlr = new MediaController(this);
         ctlr.setMediaPlayer(videoView);
@@ -64,54 +49,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void downloadFile() {
-        BaseTask.run(new BaseTask.TaskListener() {
-            @Override
-            public Object onTaskRunning(int taskId, Object object) {
-                try {
-                    URL u = new URL(videoURL);
-                    URLConnection cnx = u.openConnection();
-                    cnx.connect();
-                    int lenghtOfFile = cnx.getContentLength();
-                    InputStream is = u.openStream();
-                    FileOutputStream fos = new FileOutputStream(videoPath);
-
-                    byte data[] = new byte[1024 * 1024];
-
-                    int count = 0;
-                    long total = 0;
-                    int progress = 0;
-
-                    while ((count=is.read(data)) != -1)
-                    {
-                        total += count;
-                        int progress_temp = (int)total*100/lenghtOfFile;
-                        if(progress_temp%10 == 0 && progress != progress_temp){
-                            progress = progress_temp;
-                        }
-                        fos.write(data, 0, count);
-                    }
-
-                    is.close();
-                    fos.close();
-                } catch (IOException e) {
-                    Log.d("Error....", e.toString());
-                }
-                return null;
-            }
-            @Override
-            public void onTaskResult(int taskId, Object result) {
-                playVideo();
-            }
-            @Override
-            public void onTaskPrepare(int taskId, Object data) { }
-            @Override
-            public void onTaskProgress(int taskId, Object... values) {}
-            @Override
-            public void onTaskCancelled(int taskId) { }
-        });
-    }
-
     @Override
     protected void onResume() {
         super.onResume();
